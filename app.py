6e8474e58d1aae80fb6ac8679b3c7b8b5fd4e01a
thanishma <73327713+thanishma@users.noreply.github.com>
2023-07-07 15:44:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, Flask Docker App!'
+contacts = []
+
+@app.route('/contacts', methods=['GET'])
+def get_contacts():
+    return jsonify(contacts)
+
+@app.route('/contacts', methods=['POST'])
+def add_contact():
+    contact = {
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'phone': request.json['phone']
+    }
+    contacts.append(contact)
+    return jsonify({'message': 'Contact added successfully'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
